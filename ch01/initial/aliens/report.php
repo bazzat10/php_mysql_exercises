858ead8245cfc940b8136db711931819d33d266a
Barry Tabios <b_tabios@yahoo.com>
2017-08-10 19:32:37 +1000

    fixed aliendescription variable

--- ch01/initial/aliens/report.php
+++ ch01/initial/aliens/report.php
@@ -9,7 +9,7 @@
     <?php
       $when_it_happened = $_POST['whenithappened'];
       $how_long = $_POST['howlong'];
-      $alien_description = $_POST['description'];
+      $alien_description = $_POST['aliendescription'];
       $fang_spotted = $_POST['fangspotted'];
       $email = $_POST['email'];
 
@@ -17,7 +17,7 @@
       echo 'You were abducted ' . $when_it_happened;
       echo ' and were gone for ' .$how_long. '<br />';
       echo 'Describe them: '. $alien_description. '<br />';
-      echo 'Was Fang tehre? '.$fang_spotted. '<br />';
+      echo 'Was Fang there? '.$fang_spotted. '<br />';
       echo 'Your email address is ' . $email;
     ?>
   </body>
